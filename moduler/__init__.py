e77c83d09840b66075a3cbb2548cd6ee384599e2
Sean Fairchild <sfairchild@nexient.com>
2020-08-09 23:17:42 -0400

    init commit

---
+++ moduler/__init__.py
@@ -0,0 +1,2 @@
+def run():
+    return (u'This still works!!!')
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+        name='Moduler',
+        version='0.0.1',
+        author='Sean Fairchild',
+        author_email='seanf215@gmail.com',
+        packages=['moduler'],
+        scripts=['bin/moduler'],
+        url='#TODO Change this',
+        license='MIT',
+        description='Used to checkout packages into a directory',
+        long_description=open('README.txt').read(),
+        install_requires=[
+            "PyYAML >= 5.3.1",
+            "pytest",
+            ],
+        zip_safe=False
+        )
+
