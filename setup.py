a7b5c14ef65d03f524a67bb5460d4d447ccfbc72
Sean Fairchild <sfairchild@nexient.com>
2020-08-19 10:01:16 -0400

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='Moduler',
-        version='0.0.1',
+        version='0.0.2',
         author='Sean Fairchild',
         author_email='seanf215@gmail.com',
         packages=['moduler'],
